12af8e81b186de6f156647200bd844173645a33d
JESGOD <yonnyes@unicauca.edu.co>
2020-10-23 10:28:57 -0500

    interfaz grafica, falta programar boton enviar (insertar)

---
+++ src/entidades/departamento.java
@@ -0,0 +1,18 @@
+
+package entidades;
+
+public class departamento {
+    int NUMERO_UNICO_DEPTO;
+    String NOMBRE_DEPTO;
+   
+    public departamento() {
+        
+    }
+
+    public departamento(int NUMERO_UNICO_DEPTO, String NOMBRE_DEPTO) {
+        this.NUMERO_UNICO_DEPTO = NUMERO_UNICO_DEPTO;
+        this.NOMBRE_DEPTO = NOMBRE_DEPTO;
+    }
+
+    
+}
---
+++ src/info3_main/Info3_main.java
@@ -0,0 +1,13 @@
+
+package info3_main;
+import vista.jfrm_ui_usuario;
+
+public class Info3_main {
+
+   
+    public static void main(String[] args) {
+       jfrm_ui_usuario ventana = new jfrm_ui_usuario();
+       ventana.setVisible(true);
+    }
+    
+}
